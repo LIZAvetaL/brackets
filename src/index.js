0850ec048b701d8a55f25394cba85bdd236e7e17
lizavetal <lizaveta.levanovich@gmail.com>
2020-03-22 22:34:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let temp = [], new_str = str;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    temp.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < temp.length; j++) {
+      new_str = new_str.replace(temp[j], "");
+    }
+    if (new_str.length == 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
